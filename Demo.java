69d281669685c6300c88e3ce6cb773f0a2f4d850
yashveer007 <61536118+yashveer007@users.noreply.github.com>
2022-01-25 16:16:22 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,12 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello this is my first github program linked with jenkins ");
+		for(int i=0;i<5;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
 
 	}
 
